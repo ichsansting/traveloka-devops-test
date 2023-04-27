c465119894c4b40bd9272dd32e41314b093b8041
ichsansting <61485107+ichsansting@users.noreply.github.com>
2023-04-27 16:55:49 +0000

    add load test

---
+++ locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, TaskSet, task, between
+
+class UserTasks(TaskSet):
+    
+    @task
+    def root_page(self):
+        self.client.get("/")
+
+class WebsiteUser(HttpUser):
+    host = "http://localhost"
+    wait_time = between(1, 1)
+    tasks = [UserTasks]
