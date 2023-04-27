df0483411682acd096daaaa0d53bcb90488c3e67
ichsansting <61485107+ichsansting@users.noreply.github.com>
2023-04-27 23:19:40 +0000

    comment debug text

--- test_crud.sh
+++ test_crud.sh
@@ -8,7 +8,7 @@ create_one=$(curl -I -s --request POST http://localhost:8080/todos/create?todo=n
 [[ $create_one == 200 ]] && echo "create one todo OK" || echo "create one todo Not OK"
 
 last_create=$(curl -s --request GET http://localhost:8080/todos/create/last-id)
-echo "last create id is $last_create"
+#echo "last create id is $last_create"
 
 edit_one=$(curl -I -s --request PATCH http://localhost:8080/todos/1/edit?todo=edited%20todo | awk 'FNR==1{print $2}')
 [[ $edit_one == 200 ]] && echo "edit one todo OK" || echo "edit one todo Not OK"
